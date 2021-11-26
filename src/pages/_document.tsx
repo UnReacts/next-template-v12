4644c210d0a798570ad18f8ba97713ee42d25c08
unreact-fukumoto <fukumoto.taichi@unreact.jp>
2021-11-26 17:42:36 +0900

    Add _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html lang='ja' dir='ltr'>
+        <Head>
+          {/* サイト全体に反映させたいデフォルトの設定を記述する ex) Google Fonts の読み込み */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
