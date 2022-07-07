279b04b3cb5013f376f1a4c6f920965629d8a6de
unreact-fukumoto <fukumoto.taichi@unreact.jp>
2022-07-07 12:57:13 +0900

    ESLint 警告の修正

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,20 @@
-import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const initialProps = await Document.getInitialProps(ctx);
 
     return initialProps;
   }
 
-  render() {
+  render(): JSX.Element {
     return (
       <Html lang="ja" dir="ltr">
         <Head>
